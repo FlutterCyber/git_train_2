8b11de3cd0380e2875a8f9d27253df898265a002
flutterCyber <ronaldrask1@gmail.com>
2024-12-17 00:22:17 +0500

    added page 7

---
+++ lib/page_7.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Page7 extends StatelessWidget {
+  const Page7({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
