1cb5b93deda5a6e4042c3bdc5315677078f42bf9
MahmudjonA <abdumuratov1991@gmail.com>
2024-12-17 09:35:20 +0500

    Added page 10

---
+++ lib/page_10.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Page10 extends StatelessWidget {
+  const Page10({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
